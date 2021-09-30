2da20dc21d686f958fdbb36332646e2a9d4910d1
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-09-30 16:16:40 -0400

    proper layout change

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const input = document.querySelector('input')
+
+document.addEventListener('keydown', e => {
+  const key = e.key.toLowerCase()
+  const isSearch = key === 'f' && (e.metaKey || e.ctrlKey) // true or false if ctrl + f is pressed
+  if (!isSearch) return // if true, (!(false)) return.  else if false, (!(true)), continue
+
+  e.preventDefault()
+  input.focus()
+  console.log('focus!')
+})
